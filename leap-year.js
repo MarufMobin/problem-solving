e76ae9bf0e143ac88bc7102c7d41485b2af63163
unknown <mobinh264@gmail.com>
2021-08-05 02:03:52 +0600

    leap year three way code push

---
+++ leap-year.js
@@ -0,0 +1,53 @@
+// using condition find out leapYearYear
+
+// let numberOfYear = 	2002;
+
+// if( ( numberOfYear % 4 == 0 && numberOfYear % 100 != 0 ) || numberOfYear % 400 == 0 ){
+
+//     console.log( numberOfYear, "This Year is Leap Year");
+
+// }
+// else{
+//     console.log( numberOfYear, "This is not Leap year");
+// }
+
+
+// Anote way to find out leap year
+
+// let numberOfYear = 2021;
+
+// if( numberOfYear % 4 == 0 || numberOfYear % 400 == 0 ){
+
+//     if( numberOfYear % 100 != 0 ){
+//         console.log(numberOfYear, "This is Leap Year");
+//     }
+//     else{
+//         console.log( numberOfYear, "This is not Leap Year");
+//     }
+// }
+// else{
+//     console.log( numberOfYear, "This year is not leap year");
+// }
+
+
+
+
+// find out leap year year
+
+// function leapYear(year){
+
+//     if( ( 0 == year % 4 ) && ( 0 != year % 100 ) || ( 0 == year % 400) ){
+//         return true;
+//     }
+//     return false;
+// }
+
+// let givenYear = 2030;
+
+// let learYearResult = leapYear(givenYear);
+
+// if( learYearResult == true ){
+//     console.log( givenYear, "This year is Leap Year");
+// } else{
+//     console.log( givenYear, "This year is not leap year");
+// }
